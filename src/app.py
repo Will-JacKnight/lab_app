aaac64e8fc5219b881cf48c8ad671ecb5201124d
Guanghong Zheng <gz424@ic.ac.uk>
2024-10-28 15:45:41 +0000

    zgh5

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import re
+import math
 
 app = Flask(__name__)
 
@@ -27,6 +28,16 @@ def query():
     return process_query(query)
 
 
+def is_quare_and_cube(lt):
+    new_lt = []
+    for i in lt:
+        square_root = int(math.sqrt(i))
+        cube_root = round(abs(i) ** (1/3))
+        if square_root**2 == i and cube_root**3 == i:
+            new_lt.append(i)
+    return new_lt
+
+
 def process_query(query):
     if query == "dinosaurs":
         return "Dinosaurs ruled the Earth 200 million years ago"
@@ -43,5 +54,13 @@ def process_query(query):
     elif "minus" in query:
         numbers = [int(num) for num in re.findall(r'\b\d+\b', query)]
         return str(numbers[0]-numbers[1])
+    elif "both a square and a cube" in query:
+        numbers = [int(num) for num in re.findall(r'\b\d+\b', query)]
+        for num in numbers:
+            root = round(num ** (1 / 6)) # 6th root check
+            if root ** 2 == num ** (1 / 3) and root ** 3 == num ** (1 / 2):
+                return num
+            return "None found"
+
     else:
         return "This query is not within our test case."
